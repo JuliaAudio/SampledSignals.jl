041eb0ab8b9d58abb0a77b161acf0136a7f4b925
Spencer Russell <spencer.f.russell@gmail.com>
2015-09-23 15:48:26 -0400

    adds version check to use native Base.Test in julia 0.5

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,17 @@
-module SampleTypesTests
-
 using SampleTypes
-using BaseTestNext
 
-
-@testset "SampleTypes Tests" begin
-    include("DummySampleStream.jl")
-    include("SampleBuf.jl")
-    include("Interval.jl")
+if VERSION >= v"0.5.0-"
+    using Base.Test
+else
+    using BaseTestNext
 end
 
+try
+    @testset "SampleTypes Tests" begin
+        include("DummySampleStream.jl")
+        include("SampleBuf.jl")
+        include("Interval.jl")
+    end
+catch err
+    exit(-1)
 end
