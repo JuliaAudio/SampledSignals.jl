a97b9033c019969c886461f87f0b12dc0c2337d8
Spencer Russell <spencer.f.russell@gmail.com>
2016-01-18 14:37:35 -0500

    adds read method in terms of read!

--- src/DummySampleStream.jl
+++ src/DummySampleStream.jl
@@ -23,26 +23,28 @@ end
 
 # stream interface methods
 
-import Base.write
 """
 Writes the sample buffer to the sample sink. If no other writes have been
 queued the Sample will be played immediately. If a previously-written buffer is
 in progress the signal will be queued. To mix multiple signal see the `play`
 function. Currently we only implement the non-resampling, non-converting method.
 """
-function write{N, SR, T}(sink::DummySampleSink{N, SR, T}, buf::TimeSampleBuf{N, SR, T})
+function Base.write{N, SR, T}(sink::DummySampleSink{N, SR, T}, buf::TimeSampleBuf{N, SR, T})
     # TODO: probably should check channels here instead of using dispatch so we can give a better error message
     sink.buf = vcat(sink.buf, buf.data)
+
+    nframes(buf)
 end
 
-import Base.read
 """
 Reads from the given stream and returns a TimeSampleBuf with the data. The
 amount of data to read can be given as an integer number of samples or a
 real-valued number of seconds.
 """
-function read{N, SR, T}(src::DummySampleSource{N, SR, T}, samples::Integer)
-    retdata = src.buf[1:samples, :]
-    src.buf = src.buf[(samples+1):end, :]
-    TimeSampleBuf{N, SR, T}(retdata)
+function Base.read!{N, SR, T}(src::DummySampleSource{N, SR, T}, buf::TimeSampleBuf{N, SR, T})
+    n = min(nframes(buf), size(src.buf, 1))
+    buf.data[1:n, :] = src.buf[1:n, :]
+    src.buf = src.buf[(n+1):end, :]
+
+    n
 end
--- src/SampleStream.jl
+++ src/SampleStream.jl
@@ -14,5 +14,16 @@ Base.eltype{N, SR, T}(stream::SampleStream{N, SR, T}) = T
 
 # TODO: probably generalize to all units...
 toindex(stream::SampleSource, t::RealTime) = round(Int, t.val*samplerate(stream)) + 1
+
+# subtypes should only have to implement the `read!` and `write` methods, so
+# here we implement all the converting wrapper methods
+
 # when used as an amount of time to read, subtract one from the result of `toindex`
 Base.read(stream::SampleSource, t::RealTime) = read(stream, toindex(stream, t)-1)
+
+function Base.read{N, SR, T}(src::SampleSource{N, SR, T}, nframes::Integer)
+    buf = SampleBuf(T, SR, nframes, nchannels(src))
+    read!(src, buf)
+
+    buf
+end
