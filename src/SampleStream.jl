c68255f3d7e53f25866566e528762c572420967f
Spencer Russell <spencer.f.russell@gmail.com>
2016-02-28 22:31:40 +0100

    implements multi-to-mono mixing

--- src/SampleStream.jl
+++ src/SampleStream.jl
@@ -28,19 +28,95 @@ function Base.read{N, SR, T}(src::SampleSource{N, SR, T}, nframes::Integer)
     buf
 end
 
+const DEFAULT_BUFSIZE=4096
+
 function Base.write{N, SR, T}(sink::SampleSink{N, SR, T},
         source::SampleSource{N, SR, T},
-        bufsize=4096)
+        bufsize=DEFAULT_BUFSIZE)
     total = 0
     buf = SampleBuf(T, SR, bufsize, N)
     while true
         n = read!(source, buf)
         total += n
         if n < bufsize
+            # this currently allocates a temporary buffer, so only do it when
+            # we need to
             write(sink, buf[1:n, :])
             break
         end
         write(sink, buf)
+    end
+    
+    total
+end
+
+# handle mono-to-mono, to disambiguate between mono-to-multi and multi-to-mono
+# TODO: figure out how not to duplicate the above implementation
+function Base.write{SR, T}(sink::SampleSink{1, SR, T},
+        source::SampleSource{1, SR, T},
+        bufsize=DEFAULT_BUFSIZE)
+    total = 0
+    buf = SampleBuf(T, SR, bufsize, 1)
+    while true
+        n = read!(source, buf)
+        total += n
+        if n < bufsize
+            # this currently allocates a temporary buffer, so only do it when
+            # we need to
+            write(sink, buf[1:n])
+            break
+        end
+        write(sink, buf)
+    end
+    
+    total
+end
+
+# handle mono-to-multichannel conversion.
+function Base.write{N, SR, T}(sink::SampleSink{N, SR, T},
+        source::SampleSource{1, SR, T},
+        bufsize=DEFAULT_BUFSIZE)
+
+    total = 0
+    monobuf = SampleBuf(T, SR, bufsize, 1)
+    multibuf = SampleBuf(T, SR, bufsize, N)
+    while true
+        n = read!(source, monobuf)
+        total += n
+        for ch in 1:N
+            multibuf[1:n, ch] = monobuf[1:n]
+        end
+        write(sink, multibuf[1:n, :])
+        if n < bufsize
+            break
+        end
+    end
+    
+    total
+end
+
+# handle multi-to-mono channel conversion.
+function Base.write{N, SR, T}(sink::SampleSink{1, SR, T},
+        source::SampleSource{N, SR, T},
+        bufsize=DEFAULT_BUFSIZE)
+    if N == 0
+        error("Can't do channel conversion from a zero-channel source")
+    end
+    
+    total = 0
+    multibuf = SampleBuf(T, SR, bufsize, N)
+    monobuf = SampleBuf(T, SR, bufsize, 1)
+    while true
+        n = read!(source, multibuf)
+        total += n
+        monobuf[1:n] = multibuf[1:n, 1]
+        for ch in 2:N
+            monobuf[1:n] += multibuf[1:n, ch]
+        end
+        write(sink, monobuf[1:n])
+        if n < bufsize
+            break
+        end
     end
     
     total
--- test/SampleStream.jl
+++ test/SampleStream.jl
@@ -1,3 +1,6 @@
+# these tests use generalized SampleSink and SampleSource functionality. They
+# use Dummy sinks and sources, but all these features should be implemented
+# on the abstract Source/Sinks
 @testset "SampleStream Tests" begin
     @testset "writing sink to source" begin
         data = rand(Float32, (64, 2))
@@ -7,11 +10,20 @@
         write(sink, source, 20)
         @test sink.buf == data
     end
+
+    @testset "single-to-multi channel stream conversion" begin
+        data = rand(Float32, (64, 1))
+        source = DummySampleSource(48000, data)
+        sink = DummySampleSink(Float32, 48000, 2)
+        write(sink, source, 20)
+        @test sink.buf == [data data]
+    end
     
-    # @testset "single-to-multi channel conversion" begin
-    #     data = rand(Float32, (64, 2))
-    #     source = DummySampleSource(48000, data)
-    #     sink = DummySampleSink(Float32, 48000, 2)
-    #     
-    # end
+    @testset "multi-to-single channel stream conversion" begin
+        data = rand(Float32, (64, 2))
+        source = DummySampleSource(48000, data)
+        sink = DummySampleSink(Float32, 48000, 1)
+        write(sink, source, 20)
+        @test sink.buf == data[:, 1:1] + data[:, 2:2]
+    end
 end
