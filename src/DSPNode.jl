7944fa8cd92c26d815bfed47c2c14a3ebea22e12
Spencer Russell <spencer.f.russell@gmail.com>
2016-03-14 09:53:10 -0400

    removes DSPNode.jl file

--- src/DSPNode.jl
+++
@@ -1,5 +0,0 @@
-"""
-A signal processing node. These nodes can be wired together in a Sample
-processing graph. Each node can have a number of input and output AudioStreams.
-"""
-abstract DSPNode{SR, T <: Number}
