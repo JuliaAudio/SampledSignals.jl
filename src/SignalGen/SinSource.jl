bf9b4f5df78e6ea6f3c908d0d6c5945bea3a0869
Spencer Russell <spencer.f.russell@gmail.com>
2017-05-17 01:25:39 -0400

    adds a little more doc to SinSource [ci skip]

--- src/SignalGen/SinSource.jl
+++ src/SignalGen/SinSource.jl
@@ -1,5 +1,9 @@
 """
-SinSource is a multi-channel sine-tone signal generator.
+    SinSource(eltype, samplerate, freqs)
+
+SinSource is a multi-channel sine-tone signal generator. `freqs` can be an
+array of frequencies for a multi-channel source, or a single frequency for a
+mono source.
 """
 type SinSource{T} <: SampleSource
     samplerate::Float64
