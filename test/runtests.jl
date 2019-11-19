42732ce54b1f53d507b58fe3e3f457e6778303f7
Spencer Russell <spencer.f.russell@gmail.com>
2019-11-18 23:26:21 -0500

    Temporarily removes LibSndfile-based tests for wav display

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,10 @@ using Compat.Test
 using DSP
 using FixedPointNumbers
 using FileIO: File, Stream, @format_str
-import LibSndFile
+# for now we're disabling the wav display tests so we don't need to include
+# LibSndFile in our test dependencies. We can re-enable it once all the
+# BinaryBuilder stuff is worked out
+# import LibSndFile
 
 include("support/util.jl")
 
@@ -12,5 +15,6 @@ include("support/util.jl")
               "SampleBuf.jl",
               "SampleStream.jl",
               "SinSource.jl",
-              "WAVDisplay.jl"])
+              # "WAVDisplay.jl"
+              ])
 end
